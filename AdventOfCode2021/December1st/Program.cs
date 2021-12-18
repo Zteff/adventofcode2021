1a30e73ced596a7911f6eb5bfd2d9790aba75410
Steffen Jørgensen <dk.steffen@gmail.com>
2021-12-18 18:50:41 +0100

    December 1st part 1

---
+++ AdventOfCode2021/December1st/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace December1st
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int lastDepth = 0;
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@"depthreadings.txt"))
+            {
+                int depthInInt = int.Parse(line);
+                if(DepthIsLarger(lastDepth, depthInInt))
+                {
+                    counter++;
+                }
+
+                lastDepth = depthInInt;
+            }
+
+            Console.WriteLine("There were {0} depths increasing.", counter);
+            Console.ReadLine();
+        }
+
+        private static bool DepthIsLarger(int depth1, int depth2)
+        {
+            if (depth1 == 0)
+                return false;
+            return depth1 <= depth2;
+        }
+    }
+}
